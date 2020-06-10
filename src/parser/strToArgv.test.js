450738ef2c9ad27896d4652895f154501132b339
Philipp Wille <philipp.y.wille@gmail.com>
2020-06-10 22:45:33 +0200

    test: test if strToArgv works with string literals 1

--- src/parser/strToArgv.test.js
+++ src/parser/strToArgv.test.js
@@ -9,3 +9,14 @@ test('strToArgv works without string literals', () => {
 
   expect(res).toStrictEqual(exp)
 })
+
+test('strToArgv works with string literals 1', () => {
+  const string = 'foo "bar baz"\n'
+
+  const res = strToArgv(string)
+
+  const exp = ['foo', 'bar baz']
+
+  expect(res).toStrictEqual(exp)
+})
+
