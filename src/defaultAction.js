7cb8b4359dbbe7ab4249452c7f5f3ae9d508ed3b
Philipp Wille <philipp.y.wille@gmail.com>
2020-07-11 16:10:58 +0200

    chore: display repl usage if an unknown command is used

--- src/defaultAction.js
+++ src/defaultAction.js
@@ -1,7 +1,9 @@
-const defaultAction = (rest, errs) => {
+const {replDocs} = require('./usage')
+
+const defaultAction = commands => (rest, errs) => {
   if (errs.length > 0) {
     const err = errs.map(({code, msg}) => `${code}: ${msg}`).join('\n')
-    console.log(err)
+    console.log(`${err}\n\n${replDocs(commands)}`)
   }
 }
 
