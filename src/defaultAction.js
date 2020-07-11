3c485f20ce065734c9e43619cc707c08df3ed7e5
Philipp Wille <philipp.y.wille@gmail.com>
2020-07-11 15:44:24 +0200

    refactor: only report errors in defaultAction

--- src/defaultAction.js
+++ src/defaultAction.js
@@ -1,8 +1,5 @@
 const defaultAction = (rest, errs) => {
-  if (rest.length > 0) {
-    const err = `Unknown command "${rest[0]}"!`
-    console.log(err)
-  } else if (errs.length > 0) {
+  if (errs.length > 0) {
     const err = errs.map(({code, msg}) => `${code}: ${msg}`).join('\n')
     console.log(err)
   }
