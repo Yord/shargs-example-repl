691c1b7e970449bf569def998dd1df0b993b3902
Philipp Wille <philipp.y.wille@gmail.com>
2020-07-11 16:10:08 +0200

    chore: move help back into commands

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,7 +1,9 @@
-const {command}     = require('shargs-opts')
-const {case: _case} = require('./case')
-const {echo}        = require('./echo')
-const {help}        = require('./help')
+const {command, variadic} = require('shargs-opts')
+const {case: _case}       = require('./case')
+const {echo}              = require('./echo')
+const {cmdDocs, replDocs} = require('../usage')
+
+const help = variadic('help', ['help'], {action, desc: 'Print usage documentation.'})
 
 const opts = [
   echo,
@@ -13,4 +15,19 @@ const commands = command('shargs', opts, {desc: 'Welcome to the shargs REPL. Typ
 
 module.exports = {
   commands
+}
+
+function action (command) {
+  let help = ''
+
+  if (Array.isArray(command) && command.length === 0) {
+    help = replDocs(commands)
+  } else {
+    const command2 = Array.isArray(command) ? command : [command]
+    const subcommands = commands.opts.filter(({key}) => command2.includes(key))
+    const helps = subcommands.map(cmdDocs)
+    help = helps.join('\n')
+  }
+
+  console.log(help)
 }
